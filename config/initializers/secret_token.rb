eeee896fe0a04b6e7ae6d854a24348343c2e4947
batson456 <batson456@gmail.com>
2012-08-08 18:28:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Katy::Application.config.secret_token = 'ec0e9507f3c32d5090e045f5cff7f3d40b2da22072964cc9065b45f5899bf2316e64e68745f25d8b98ec7b8e8052c0745ae88390314ef71053f3adadcc7d76af'
