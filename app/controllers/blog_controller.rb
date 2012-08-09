c5315850e7eea335f71bfddf5389b69c00647b17
batson456 <batson456@gmail.com>
2012-08-08 20:24:07 -0400

    First start, working view file

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,6 @@
 class BlogController < ApplicationController
   def index
+  	@posts = Post.paginate(:page => params[:page])
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :tags, :title
+  validates :content, :tags, :title, :presence => true
+
 end
