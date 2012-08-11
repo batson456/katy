d409949e413a573082524b2848d3bade3ee837f0
batson456 <batson456@gmail.com>
2012-08-11 09:39:11 -0400

    Fully merge active_admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Post do
   form do |f|
       f.inputs "Details" do
         f.input :title
-        f.input :image, :label => "Main Image"
+        f.input :image, :as => :file, :label => "Main Image"
       end
       f.inputs "Content" do
         f.input :content, :input_html => {:class => "ckeditor"}
