986879218fedacbb6eb5649f5bb2c97317e9343c
batson456 <batson456@gmail.com>
2012-08-14 19:24:35 -0400

    Display image only if it exists

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :tags, :title, :image
   validates :content, :title, :presence => true
 
-  has_attached_file :image, :styles => { :small => "300x770>"},
+  has_attached_file :image, :styles => { :small => "300x770#"},
   									:url => '/assets/posts/:id/:style/:basename.:extension',
   									:path => ':rails_root/public/assets/posts/:id/:style/:basename.:extension'
 end
