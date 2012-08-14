f523a0c158b35a1d734642788fab2410db640449
batson456 <batson456@gmail.com>
2012-08-14 19:42:57 -0400

    Fix minor error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :tags, :title, :image
   validates :content, :title, :presence => true
 
-  has_attached_file :image, :styles => { :small => "200x770#"},
+  has_attached_file :image, :styles => { :small => "770x225#"},
   									:url => '/assets/posts/:id/:style/:basename.:extension',
   									:path => ':rails_root/public/assets/posts/:id/:style/:basename.:extension'
 end
