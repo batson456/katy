7d1ee924d327f84e58849b180beeb9de36efa770
batson456 <batson456@gmail.com>
2012-08-14 17:54:00 -0400

    Lots of styling and imagemagick resize and upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :tags, :title, :image
   validates :content, :title, :presence => true
 
+  has_attached_file :image, :styles => { :small => "300x770>"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120814213131) do
+ActiveRecord::Schema.define(:version => 20120814213233) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -80,8 +80,12 @@ ActiveRecord::Schema.define(:version => 20120814213131) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
