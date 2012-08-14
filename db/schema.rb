fe33762d4a30753e2b6950119d3dc10f3471227f
batson456 <batson456@gmail.com>
2012-08-14 17:31:54 -0400

    Remove image column from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120812001739) do
+ActiveRecord::Schema.define(:version => 20120814213131) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(:version => 20120812001739) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "image"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
