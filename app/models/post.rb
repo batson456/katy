4f20ce5501b684b37f9fbfe30e7d2a3f312cc5cd
batson456 <batson456@gmail.com>
2012-08-28 22:30:18 -0400

    Change thumbnails thing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :tags, :title, :image
   validates :content, :title, :presence => true
 
-  has_attached_file :image, :styles => { :small => "770x225#"},
-  									:url => '/assets/posts/:id/:style/:basename.:extension',
-  									:path => ':rails_root/public/assets/posts/:id/:style/:basename.:extension'
+  has_attached_file :image, :styles => { :small => "770x225#"}
 end
